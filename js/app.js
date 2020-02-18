96dfcb646c748efe000ec7b6734c676b1d799b3c
elvira <elvirapedregal@hotmail.es>
2020-02-18 13:35:52 +0100

    first-commit

---
+++ js/app.js
@@ -0,0 +1,26 @@
+//objeto
+
+let rover = {
+  name: 'rover',
+
+  cordenates: [],
+  
+}
+
+rover.cordenates.push [""]
+
+
+
+
+turnLeft = function(rover){
+
+}
+
+turnRight= function(rover){
+
+}
+
+
+Foward = function(rover){
+
+}
